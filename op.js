8ebb3459d5f9ba769662465d3e15bb980965523e
vishnupriya760 <vishnupriyaot7@gmail.com>
2025-06-23 17:18:38 +0530

    Add files via upload

---
+++ op.js
@@ -0,0 +1,280 @@
+
+
+
+const coin = 'head'
+
+if(coin=='tail'){
+    console.log('csk won the toss');
+    
+}
+
+
+let syspass='jona123'
+
+let userEnteringpassword='jona123'
+
+
+if(syspass==userEnteringpassword){
+    console.log('Welcome, system loggged in');
+    
+}else{
+    console.log('invalid password');
+    
+}
+
+//theme park age above 18
+
+
+
+
+//find the given number is even or odd
+//leap year or not
+
+//declar two numbers num, num2, opertation ='sum'
+//num, num  num2 greater than num num/n2    n2/n1
+
+let num=5;
+if(num%2==0){
+    console.log(num+"is even");
+    
+}else{
+    console.log(num+"is odd");
+    
+}
+
+
+
+let no=5;
+let no2=7;
+let opertation='sum';
+if(no =="sum"){
+    console.log(no+no2);
+    
+}else{
+    console.log(no-no2);
+    
+}
+let numb1=10;
+let numb2=20;
+if(numb1>numb2){
+    console.log("numb1 is greater ");
+    console.log("numb2/numb1=",numb1/numb2);
+  
+}else{
+    console.log("numb2 is greater");
+    console.log("numb1/numb2=",numb2/numb1);
+}
+
+// check if a number is positive or negative
+let number=-10;
+if(number >0){
+    console.log("positive number");
+}else 
+{
+    console.log("negative number");
+}
+
+//find largest number
+let x=10;
+let y=20;
+if(x>y){
+    console.log("x is largest")
+}else(x<y)
+{
+    console.log("y is smallest");
+}
+
+let yr=2020;
+let=4;
+ if(yr%2==0){
+    console.log('leapyear');
+    
+ }else{
+    console.log('not  leapyear');
+    
+ }
+
+//logical operator  and -&& or-||  not-!
+
+let username='vishnu'
+let password=1234568
+
+// if (username=='vishnu' && password==1234568){
+//     console.log('login successfull');
+    
+// }else{
+//     console.log('login failed');
+    
+// }
+
+
+if (username=='vishnu' && password==12348){
+    console.log('login successfull');
+    
+}else{
+    console.log('login failed');
+    
+}
+
+
+//! true - false  --> false - true
+
+if(!username=='vishnu'){
+    console.log('this is vishnu');
+    
+}else{
+    console.log('this is not vishnu');
+    
+}
+
+let  Vage=60;
+if(Vage>0 && Vage<18){
+    console.log('not eligible for vote');
+    
+}else{
+    console.log('eligible for vote');
+    
+}
+//  task1
+let email='vp123'
+let pass=8428
+
+if (email =='vp123' && password=='8428'){
+    console.log('login successfully');
+    
+}else{
+    console.log('login failed');
+    
+}
+
+//task3
+   let ticket= true;
+   if( ticket){
+    console.log('access allowed');
+    
+   }else{
+    console.log('access not allowed');
+    
+   }
+    // task2
+        let sub1=50;
+        let sub2=40;
+        if(sub1>0 && sub2<40){
+            console.log('pass');
+            
+        }else{
+            console.log('fail');
+            
+        }
+
+
+        //0 > positive  <neg 0
+
+    let num1=0
+
+    if(num1>0){
+        console.log('this is positive number')
+        
+    }else if(num1<0){
+                console.log('this is neg number')
+
+    }else{
+        console.log('this numbr is 0')
+        
+    }
+
+
+    // the give mark is 0- 35 fail, 35-70-avg 70-100 outstaning, invalid marks
+
+    let mark=101
+    if(mark<35 && mark>=0){
+        console.log('fail')
+        
+    }else if(mark>=35 && mark<70){
+      console.log('avg')
+      
+    }else if(mark>=70 && mark<=100){
+        console.log('outstanding')
+        
+    }else{
+        console.log('invalid marks');
+        
+    } 
+    //optn = add , sub, div, mul, reminder....invalid
+    
+     let nu=25;
+     let nu2=5;
+     let operation="remainder";
+     if(operation==="add"){
+        console.log("result:",nu+nu2);
+     }else if(operation==="subtract"){
+        console.log("result:",nu-nu2);
+        
+     }else if(operation==="multiply"){
+        console.log("result:",nu*nu2);
+        
+     }else if( operation==="division"){
+        console.log("result:",nu/nu2);
+        
+     }else if(operation==="remainder"){
+        console.log("result:",nu%nu2);
+        
+     }else {
+        console.log('invalid');
+        
+     }
+     
+    
+    //find the ticknt pric ==> >10 ==pric 40, 10 -15 = 60, above 15- 60 150 --- abov 60 ---30
+let ag=2;
+let price;
+if(ag>0 && ag< 10){
+    console.log(price='40');
+    
+}else if(ag>=10 && ag<=15){
+   console.log(price='60');
+    
+   }else if(ag>15 && ag<=60){
+   console.log( price='150');
+}else if(ag>60){
+    console.log(price='30');   
+    
+}
+
+
+ console.log("ticket price:",price);
+
+    
+
+
+let yer=2024;
+if(yr% 4=== 0){
+    if(yr % 100 ===0){
+        if ( yr % 400 ===0){
+            console.log(yr +" is a leap year");
+            
+        }else{
+            console.log(yr+ "is not a leap year");
+        
+        }
+        }else{
+            console.log(yr + "is a leap year");
+            
+        }
+
+        }else{
+            console.log(yr+"is not a leapyear");
+            
+        }
+    
+
+
+
+
+
+    
+
+    
+
+
+
---
+++ switch.js
@@ -0,0 +1,115 @@
+let color='grn'
+
+switch(color){
+    case 'red':
+        console.log('stop');
+        break;
+
+    case 'green':
+        console.log('go');
+        break;
+    case 'yello':
+        console.log('wait');
+        break;
+    default:
+        console.log('colors doesnt matched try another color');
+        break;
+        
+      
+}
+
+
+// task1
+let socialmedia='facebook'
+    switch(socialmedia){
+        case 'instagram':
+            console.log( 'Share what you into with the people who get you');
+            break;
+            case 'facebook':
+                console.log(' Connect with friends, family and other people you know');
+                break;
+                case 'whatsapp':
+                    console.log('With private messaging and calling');
+                    break;
+                    default:
+                        console.log('its not socialmedia');
+                        break;
+    }
+//task2
+let season='spring'
+ switch(season){
+    case 'summer':
+        console.log('Ac');
+        break;
+        case 'rainy':
+            console.log('umberlla');
+            break;
+            case 'spring':
+                console.log('flowers');
+                break;
+                case 'autumn':
+                    console.log('leaf ')
+                    break;  
+ }
+ //task3
+ let country="korea"
+   switch (country){
+    case 'japan':
+        console.log('anime');
+        break;
+       case 'korea':
+        console.log('kpop');
+        break;
+        case 'india':
+            console.log('biriyani');
+            break;
+            case 'china':
+                console.log('great wall');
+                break;
+                
+                    
+        
+   }
+//task4
+   let operation="mul"
+    switch (operation){
+        case 'sum':
+        num1=7;
+        num2=6;
+        console.log('13');
+        break;
+        case 'sub':
+            num1=7;
+            num2=6;
+            console.log('-1');
+            break;
+            case 'mul':
+                num1=7;
+                num2=6;
+                console.log('42');
+                break;         
+    }
+
+    // eve task
+
+    let seasons='summer'
+    switch (season){
+        case 'summer':
+            console.log('6,7,8');
+
+            break;
+            case 'winter':
+                console.log('12,1,2');
+                break;
+            case 'spring':
+                console.log('3,4,5');
+                break;
+                case 'autumn':
+                    console.log('9,10,11');
+                    break;
+                    
+                
+
+
+
+    }
