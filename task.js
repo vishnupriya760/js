42327f696bd225d32754362a10cfa9332eea8005
vishnupriya760 <vishnupriyaot7@gmail.com>
2025-06-24 15:37:00 +0530

    Add files via upload

---
+++ task.js
@@ -0,0 +1,72 @@
+let yer=2024;
+if(yr% 4=== 0){
+    if(yr % 100 ===0){
+        if ( yr % 400 ===0){
+            console.log(yr +" is a leap year");
+            
+        }else{
+            console.log(yr+ "is not a leap year");
+        
+        }
+        }else{
+            console.log(yr + "is a leap year");
+            
+        }
+
+        }else{
+            console.log(yr+"is not a leapyear");
+            
+        }
+    
+
+          // eve task
+
+let yr=2024;
+if(yr% 4=== 0){
+    if(yr % 100 ===0){
+        if ( yr % 400 ===0){
+            console.log(yr +" is a leap year");
+            
+        }else{
+            console.log(yr+ "is not a leap year");
+        
+        }
+        }else{
+            console.log(yr + "is a leap year");
+            
+        }
+
+        }else{
+            console.log(yr+"is not a leapyear");
+            
+        }
+    
+// task2
+let a=10;
+let b= 15;
+let c=25;
+if (a>b &&a>c){
+    console.log("largest is:",+a);
+    
+}else if(b<c && b<c){
+    console.log("largest is :",+b);
+    
+    
+}
+
+
+
+
+    
+
+    
+
+
+
+
+
+
+
+    
+
+    
