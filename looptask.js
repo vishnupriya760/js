224092b3157ad1f50be27a15ae44f6f7e034341e
vishnupriya760 <vishnupriyaot7@gmail.com>
2025-06-24 15:36:24 +0530

    Add files via upload

---
+++ looptask.js
@@ -0,0 +1,96 @@
+// task1
+// odd square
+n=1
+while(n<=15){
+    console.log(n**2);
+     n+=2
+    
+}
+
+
+// task2
+// 3,5,7,...30
+ let num=3
+while(num<=30){
+    console.log(num);
+
+    num+=2
+    
+}
+
+
+
+// task3
+// 5,10,15,20,...50
+let nu=5
+while(nu<=50){
+    console.log(nu);
+
+    nu+=5
+    
+}
+// task4
+let no=1
+while(no<=10){
+    console.log(no*2);
+    no++
+}
+let sum=0
+
+
+let nd=1
+while(nd<=5){
+sum+=nd
+
+
+
+    nd++
+}
+
+
+
+// task5
+console.log(sum);
+
+let res =1
+
+let d=1
+while(d<=5){
+    
+
+    res*=d
+    console.log(res);
+    d++
+}
+
+
+
+// task6
+y=1
+k=5
+while(y<=10){
+    console.log(`${y}*${k}=${k*y}`);
+    y++
+    
+}
+let ci=1
+while(ci<=15){
+    
+if(ci%3==0){
+    console.log('fizz');
+    
+
+}
+else if(ci%5==0){
+    console.log('buzz');  
+}
+else{
+ console.log(ci);
+ 
+}
+ci++
+ }
+
+    
+
+
