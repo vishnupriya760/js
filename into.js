892dd35a23fb3941b3f1f6010300cc3207647c5c
vishnupriya760 <vishnupriyaot7@gmail.com>
2025-06-24 15:35:53 +0530

    Add files via upload

---
+++ into.js
@@ -0,0 +1,22 @@
+// we can use camelcase
+
+
+let firstName='vishnu'
+let secondName='priya'
+
+
+
+// variables keywords => let , var , const
+//  datatypes 1. primitive and non primitive
+
+// 1. primitive-number,string, Boolean, null, undefined
+// 2. nonprimitive - object,array ,date
+
+let age=20;
+let username="vjhk`125"
+
+let show=true;
+let bal= null;
+
+let address;
+ 
