726cc9aabe8aeac587931f75e3b62ac6de7f0ebf
vishnupriya760 <vishnupriyaot7@gmail.com>
2025-06-24 15:36:10 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,28 @@
+//initialization, condition , increment or decrement
+
+
+
+let count=0
+
+while(count<=20){
+    console.log(count);
+    
+
+
+    count++
+
+}
+sqr=1
+while(sqr<=5){
+    console.log('square of a number', sqr**2);
+    sqr++
+}
+
+let numb=0
+while(numb<=20){
+    console.log(numb);
+    numb+=2
+    
+}
+
+
