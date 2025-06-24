34e9ccacf90016995eb35a2d73ade90143e8b6e4
vishnupriya760 <vishnupriyaot7@gmail.com>
2025-06-24 15:35:07 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,34 @@
+let age=20;
+let dateOfBirth=12;
+let date=14;
+let number=22;
+let ages=25;
+
+let username="vishnu"
+let name="anu"
+let id ="priya"
+let animal="dog"
+let bird="parrot"
+
+let account=null;
+let password=null;
+let cart=null;
+let balance=null;
+let saving=null;
+
+
+let pet=true;
+let color=false;
+let passedout=true;
+let money=true;
+let item=false;
+
+
+ let coding;
+ let numbers;
+ let variables;
+ let alphabets;
+ let conditions;
+
+
+ 
