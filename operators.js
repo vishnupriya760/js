2ab3b31667e73f8dee07b53774011a706e5ce4f8
vishnupriya760 <vishnupriyaot7@gmail.com>
2025-06-24 15:36:43 +0530

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,65 @@
+// arithmetic operators  + - / * %(remainder) **(POWER)
+
+let a=10;
+let b=3
+
+let result= a +b ;
+console.log(result)
+console.log(a%b)
+
+console.log(2**3)
+
+
+//comparison operator > < >= <= == === != !==
+
+let vmark=56;
+let jmark=67;
+
+console.log(vmark>jmark)
+
+console.log(vmark=='56'); 
+
+console.log(vmark==='56');
+// ===strict equality , it will check values with datatype
+
+
+c=10;
+console.log(a==c);
+console.log(a!=b);
+
+
+
+
+// find area of circle  2*3.14*r
+
+
+let r= 13;
+
+let result1 =  2*3.14*r
+
+console.log(result1);
+
+
+//find the area of rectangle (h, l)
+//si  p n r /100
+//area of cylinder
+//bmi calculate
+//find goldrate according to gram
+
+
+
+//declare 5subject marks ==> compare tamil>english
+
+//assignment operator - ++n , n++ (increment operator), n-- , --n
+
+let valu=10;
+valu+=5
+console.log(valu);
+
+// incrementing or decrementing by 1 means n++ n--
+// more than 1 means n+=5 --->n=n+5
+
+
+
+
+
